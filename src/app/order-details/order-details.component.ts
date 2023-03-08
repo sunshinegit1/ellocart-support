287f5c5739258d087b18ddecd063051d1da6ebe1
Roop Sagarh Talari <git@sunshinetech.info>
2023-03-08 18:32:42 +0530

    order details changes

--- src/app/order-details/order-details.component.ts
+++ src/app/order-details/order-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonModal } from '@ionic/angular';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -8,9 +9,55 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./order-details.component.scss'],
 })
 export class OrderDetailsComponent implements OnInit {
+  @ViewChild(IonModal) modal!:IonModal;
   userData: any;
   oid: any;
   orderData:any=[];
+  remove=false;
+  products=[
+    {
+      id:1,
+      name:'p1',
+      price: 245,
+      quantity: 2,
+      selected:true
+    },
+    {
+      id:2,
+      name:'p2',
+      price: 230,
+      quantity: 1,
+      selected:true
+    },
+    {
+      id:3,
+      name:'p3',
+      price: 150,
+      quantity: 4,
+      selected:true
+    },
+    {
+      id:4,
+      name:'p4',
+      price: 120,
+      quantity: 2,
+      selected:false
+    },
+    {
+      id:5,
+      name:'p5',
+      price: 350,
+      quantity: 1,
+      selected:false
+    },
+    {
+      id:6,
+      name:'p6',
+      price: 400,
+      quantity: 4,
+      selected:false
+    }
+  ];
 
   constructor(
     private api:ApiService,
@@ -29,4 +76,23 @@ export class OrderDetailsComponent implements OnInit {
       this.orderData = res.ResultSet;
     })
   }
+  editProducts(){
+    this.remove = true;
+  }
+  removeProduct(i: any){
+    this.products.splice(i,1);
+  }
+
+  //modal
+  cancel() {
+    this.modal.dismiss();
+  }
+
+  confirm() {
+    this.modal.dismiss();
+  }
+  searchProduct(e:any){
+    console.log(e.detail.value);
+  }
+
 }
